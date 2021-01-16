c04271440511f404121a34f8a538d6732ade8896
luaygharzeddine <luaygharzeddine@gmail.com>
2021-01-16 22:48:13 +0200

    Added comment for clarity

--- rhythmbox_alarm.py
+++ rhythmbox_alarm.py
@@ -110,7 +110,7 @@ start_volume = 0.5
 end_volume = 1.0
 duration = 200 # In seconds, how long it takes to go from `start_volume` to `end_volume`
 
-alarm_time = sys.argv[1]
+alarm_time = sys.argv[1] # A string formatted as HH:MM
 
 # Reset
 stop()
